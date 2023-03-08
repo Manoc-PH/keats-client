0e623b5c316027811a34d4490f11e5d1526911f8
cloyd <abadcloyd@gmail.com>
2023-03-08 11:49:01 +0800

    added axios interceptor

--- common/utils/axios.js
+++ common/utils/axios.js
@@ -8,32 +8,32 @@ import { BASE_URL, LOCAL_URL } from "@app/common/constants/APIUrls";
 import { ClearCredentials } from "@app/services/db";
 
 // Store
-import { actions } from "@app/core/store";
+import { actions, reducers } from "@app/core/store";
 
 export const authAxios = axios.create({
   baseURL: LOCAL_URL,
   withCredentials: true,
 });
 // Logging out user if token expires
-// authAxios.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     // Store Actions
-//     // TODO CHECK IF THIS WORKS
-//     // const { setIsLoggedIn: setIsLoggedInState } = actions;
-//     // const dispatch = useDispatch();
-//     // const setIsLoggedIn = (v) => dispatch(setIsLoggedInState(v));
-//     if (error.response.status === 401 || error.response.status === 403) {
-//       ClearCredentials()
-//         .then(() => {
-//           // setIsLoggedIn(false);
-//           return Promise.reject(error);
-//         })
-//         .catch((err) => console.log(err));
-//     }
-//     return Promise.reject(error);
-//   }
-// );
+authAxios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    // Store Actions
+    // TODO Add logout function when token expires
+    // const { setIsLoggedIn: setIsLoggedInState } = actions;
+    // const dispatch = useDispatch();
+    // const setIsLoggedIn = (v) => dispatch(setIsLoggedInState(v));
+    if (error.response.status === 401 || error.response.status === 403) {
+      ClearCredentials()
+        .then(() => {
+          // setIsLoggedIn(false);
+          return Promise.reject(error);
+        })
+        .catch((err) => console.log(err));
+    }
+    return Promise.reject(error);
+  }
+);
 
 // TODO: REVERT THIS BACK TO THE REAL SERVER
 export const publicAxios = axios.create({
