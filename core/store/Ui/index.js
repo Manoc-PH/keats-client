5ed9830459d9e946401816621bbc53edfda987b7
Cloyd Abad <abadcloyd@gmail.com>
2023-06-19 21:06:40 +0800

    fixed view simple not working

--- core/store/Ui/index.js
+++ core/store/Ui/index.js
@@ -10,7 +10,7 @@ const store = createSlice({
   reducers: {
     setIsViewSimple: (state, { payload }) => {
       if (payload) state.isViewSimple = payload;
-      else state.isViewSimple = true;
+      else state.isViewSimple = false;
     },
   },
 });
